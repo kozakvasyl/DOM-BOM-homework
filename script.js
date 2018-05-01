68470543b505706103df590920dcbf4809f062d6
kozakvasyl <koretsua@gmail.com>
2018-05-01 04:07:31 +0300

    some modified into script.js file

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-/*var block1 = document.getElementById('block1');
-console.log(block1);*/
-
-/*var block = document.getElementsByClassName('block');
-console.log(block);*/
-
-/*var i =0;
-while (i < block.length) {
-	console.log(block[i]);
-	i++;
-}*/
-
-/*var main = document.getElementById('main');
-console.log(main.childNodes);*/
-
-
-/*console.log (navigator.userAgent);
-console.log (navigator.platform);
-console.log (screen);
-console.log (location);*/
-
-/*var html = '<form name="login" action="google.com" style="padding: 10px; align-self: center;">\
-   <input type="text" name="age" style="height: 30px; margin-left: 10px">\
-   <input type="text" name="username" style="height: 30px; margin-left: 10px">\
-   <input type="text" name="date" style="height: 30px; margin-left: 10px">\
-   <input type="submit" value="Validate Me" style="height: 30px; margin-left: 10px">\
-</form>'*/
-
-
-
-/*var appendForm = document.getElementById('block1');
-var html = '<form name="login" action="google.com" style="padding: 10px; align-self: center;">\
-   <input type="text" name="age" style="height: 30px; margin-left: 10px">\
-   <input type="text" name="username" style="height: 30px; margin-left: 10px">\
-   <input type="text" name="date" style="height: 30px; margin-left: 10px">\
-   <input type="submit" value="Validate Me" style="height: 30px; margin-left: 10px; background: #104d62;">\
-</form>'
-appendForm.insertAdjacentHTML('beforeEnd', html);
-
-console.log(html); */
 
 var block1 = document.querySelectorAll('#block1')[0];
 var form = document.createElement('form');
@@ -50,21 +10,21 @@ block1.insertAdjacentElement('beforeend', form);
 var age = document.createElement('input');
 age.type= "text";
 age.name= "age";
-age.placeholder = "age";
+age.placeholder = "your age";
 age.className = "inputStyle";
 form.insertAdjacentElement('beforeEnd', age);
 
 var username = document.createElement('input');
 username.type="text";
 username.name="username";
-username.placeholder = "_@gmail.com";
+username.placeholder = "your username";
 username.className = "inputStyle";
 form.insertAdjacentElement('beforeEnd', username);
 
 var date = document.createElement('input');
 date.type="text";
 date.name="date";
-date.placeholder = "curent date";
+date.placeholder = "dd/mm/yyyy (date)";
 date.className = "inputStyle";
 form.insertAdjacentElement('beforeEnd', date);
 
@@ -74,41 +34,55 @@ submit.value="Validate Me";
 submit.className = "submitStyle";
 form.insertAdjacentElement('beforeEnd', submit);
 
-<<<<<<< HEAD
-console.log (age.value);
+var success = 0;
+var regexAge = /()\d/g;
+var regexUsername = 'user_';
+var regexDate = /\d{2}\/\d{2}\/\d{4}/;
 
-var regexAge = /\d/g;
-if ( regexAge.test(age) )
-   console.log (true);
-else
-   console.log(false);
-
-
-
-=======
-form.addEventListener('submit', validate);
 
 function validate(e) {
    e.preventDefault();
 
-   var regexAge = /\d/g;
-   if( regexAge == age.value || age.value >= 0 )
-      console.log(true);
+   if(age.value === regexAge || age.value >= 0)
+      /*success++;*/
+      console.log(success++);
    else
-      console.log(false);
+      age.style.border = '1px solid red'
+      setTimeout( function(){age.style.border = '1px solid #0000003b'}, 3000);
+
+   if (regexUsername == username.value)
+      console.log(success++);
+   else
+      username.style.border = '1px solid red'
+      setTimeout( function(){username.style.border = '1px solid #0000003b'}, 3000);
+
+      if (regexDate == date.value)
+      console.log(success++);
+   else
+      date.style.border = '1px solid red'
+      setTimeout( function(){date.style.border = '1px solid #0000003b'}, 3000);
+
+
+   if (success === 3) 
+    window.open('https://www.google.com/');
+
+      console.log (success);
 }
-console.log (typeof " ");
->>>>>>> 3755886f41beb9c20ba6b78da1957f0b9e614cad
+
+form.addEventListener('submit', validate);
 
 
 
 // ----- get curent year -----
- function clock () {
+/* function clock () {
  	var time = new Date();
  	var year = time.getFullYear();
  	document.querySelectorAll('.bottom')[0].innerHTML =year+' ©   kozakvasyl';
  }
- clock();
+ clock();*/
+
+ var time = new Date();
+ console.log(time);
 
  
 
