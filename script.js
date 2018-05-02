40f084a954cfcb2749cb2083b498e14120d6e267
kozakvasyl <koretsua@gmail.com>
2018-05-02 04:13:13 +0300

    added task3

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-var block1 = document.querySelectorAll('#block1')[0];
+// ----- TASK1 validation -----
+/*var block1 = document.querySelectorAll('#block1')[0];
 var form = document.createElement('form');
 form.name="login";
 form.action="google.com";
 form.className = "validate";
 block1.insertAdjacentElement('beforeend', form);
-
 
 var age = document.createElement('input');
 age.type= "text";
@@ -24,7 +23,7 @@ form.insertAdjacentElement('beforeEnd', username);
 var date = document.createElement('input');
 date.type="text";
 date.name="date";
-date.placeholder = "dd/mm/yyyy (date)";
+date.placeholder = "dd-mm-yyyy (date)";
 date.className = "inputStyle";
 form.insertAdjacentElement('beforeEnd', date);
 
@@ -35,55 +34,91 @@ submit.className = "submitStyle";
 form.insertAdjacentElement('beforeEnd', submit);
 
 var success = 0;
-var regexAge = /()\d/g;
-var regexUsername = 'user_';
-var regexDate = /\d{2}\/\d{2}\/\d{4}/;
-
+var regexAge = /\d/g;
+var regexUsername = /user_/;
+var regexDate = /\d{2}-\d{2}-\d{4}/;
 
 function validate(e) {
    e.preventDefault();
 
-   if(age.value === regexAge || age.value >= 0)
-      /*success++;*/
-      console.log(success++);
+   if(regexAge.test(age.value) && age.value >= 0) {
+      success++;
+      console.log(true);
+   }
+      
    else
-      age.style.border = '1px solid red'
+      age.style.border = '1px solid red';
       setTimeout( function(){age.style.border = '1px solid #0000003b'}, 3000);
 
-   if (regexUsername == username.value)
-      console.log(success++);
+   if( regexUsername.test(username)) {
+      success++;
+   }
+   else if (!username.value) {return false;}
    else
-      username.style.border = '1px solid red'
+      username.style.border = '1px solid red';
       setTimeout( function(){username.style.border = '1px solid #0000003b'}, 3000);
 
-      if (regexDate == date.value)
-      console.log(success++);
+   if(regexDate.test(date)) {
+      success++;
+   }
+   else if (!date.value) {return false;}
+   
    else
-      date.style.border = '1px solid red'
+      date.style.border = '1px solid red';
       setTimeout( function(){date.style.border = '1px solid #0000003b'}, 3000);
+      alert('Your date is invalid');
+      
 
-
-   if (success === 3) 
-    window.open('https://www.google.com/');
-
-      console.log (success);
+   if(success === 3) { 
+      window.open('https://www.google.com/');
+   }
 }
 
-form.addEventListener('submit', validate);
+form.addEventListener('submit', validate);*/
 
+
+
+
+// ----- TASK3 user system -----
+
+var button = document.querySelectorAll('.button')[0];
+button.addEventListener('click', showPlatform);
+
+function showPlatform () {
+
+   var platform = navigator.platform.toLowerCase();
+
+   var platformsMap = {
+      'mac': 'macos',
+      'iphone': 'macos',
+      'win': 'windows',
+      'linux': 'linux'
+   };
+
+   for (var key in platformsMap) {
+      key = key.toLowerCase();
+      var version = platformsMap[key];
+     
+     if (platform.indexOf(key) >= 0){
+       
+          var message = document.getElementsByClassName('download-message');
+            
+        for (var i = 0; i <= message.length; i++) {
+            
+            if ( message[i].getAttribute('data-version') === version ) {
+               
+               message[i].style.display='block';
+            }
+         }
+      }
+   }
+}
 
 
 // ----- get curent year -----
-/* function clock () {
+ function clock () {
  	var time = new Date();
  	var year = time.getFullYear();
  	document.querySelectorAll('.bottom')[0].innerHTML =year+' ©   kozakvasyl';
  }
- clock();*/
-
- var time = new Date();
- console.log(time);
-
- 
-
-
+ clock();
