fa83d406d645b3af9182f7967ba5615ac12c09ff
kozakvasyl <koretsua@gmail.com>
2018-05-10 02:39:28 +0300

    added paralax-efect to a header

--- scripts/paralax.js
+++ scripts/paralax.js
@@ -2,6 +2,6 @@
 window.addEventListener('scroll', function (e) {
   console.log(document.body.scrollTop);
   var $parallax = document.getElementsByTagName('header')[0];
-  var posY = window.pageYOffset * 1.7;
-  $parallax.style.backgroundPosition = '1px ' + (-posY) + '1px';
+  var posY = window.pageYOffset;
+  $parallax.style.backgroundPositionY = -posY + 'px';
 });
