84f2783e9831fcfd732fb3532ddbdd0ebbbffebc
kozakvasyl <koretsua@gmail.com>
2018-05-12 04:32:09 +0300

    added prevent task

--- scripts/prevent.js
+++ scripts/prevent.js
@@ -1,14 +1,30 @@
-var onClick = function (e) {
+var stopPropOn = function (e) {
 	var $el = e.target;
-  
-  e.stopPropagation();
-	
-  $el.style.backgroundColor = 'yellow';
+	e.stopPropagation();
+	$el.style.backgroundColor = 'yellow';
 	alert('CLICK!');
-    $el.style.backgroundColor = '';
 }
+box1.onclick = stopPropOn;
+box2.onclick = stopPropOn;
+box3.onclick = stopPropOn;
 
-/*document.body.onclick = onClick;*/
-box1.onclick = onClick;
-box2.onclick = onClick;
-box3.onclick = onClick;
+var stopPropOf = function (e) {
+	var $el = e.target;
+	$el.style.backgroundColor = 'yellow';
+	alert('CLICK!');
+}
+box4.onclick = stopPropOf;
+box5.onclick = stopPropOf;
+box6.onclick = stopPropOf;
+
+var preventDefOn = function (e) {
+	var $el = e.target;
+	e.preventDefault();
+}
+button1.onclick = preventDefOn;
+
+var preventDefOf = function (e) {
+	var $el = e.target;
+}
+button2.onclick = preventDefOf;
+
