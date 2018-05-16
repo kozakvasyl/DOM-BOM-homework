f6307359410e04c42b6cb746aa9c2fc861d5901a
kozakvasyl <koretsua@gmail.com>
2018-05-16 03:11:35 +0300

    added changes to task4

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,18 +1,25 @@
 
 var applyLanguage = function (lang) {  
 	alert('now language is: ' + lang);
+	var lang = document.getElementsByName('lang');
+		for (var i=0; i<lang.length; i++) {
+			if (lang[i].checked) {
+				localStorage.setItem(lang[i].value);
+			}
+		}
 }
+
 
 var getCurrentLanguage = function () {
 	var defaultLanguage = 'ua';
-	//
+	//localStorage.getItem();
   
   	return defaultLanguage;
 }
 
 var currentLang = getCurrentLanguage();
 var langEls = document.getElementsByClassName('lang-' + currentLang);
-//console.log(langEls);
+
 
 for (var i=0; i<langEls.length; i++) {
 	var langEl = langEls[i];
@@ -27,7 +34,6 @@ for (var i=0; i<langEls.length; i++) {
 
 // $<prefix> == DOMElement
 var $save = document.querySelector('#save');
-console.log($save);
 $save.addEventListener('click', function(){
 	alert(1);
 });
