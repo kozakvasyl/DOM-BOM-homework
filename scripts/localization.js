eed21e178c107a53390d691075524b7a535b3b97
kozakvasyl <koretsua@gmail.com>
2018-05-17 02:23:16 +0300

    changed localization.js file and added some change to html and css

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,21 +1,35 @@
+var lang = document.getElementsByName('lang');
+		for (var i=0; i<lang.length; i++) {
+			lang[i].onchange = applyLanguage;
+		}
 
-var applyLanguage = function (lang) {  
+function applyLanguage() {
+	localStorage.setItem('myKey', this.value);
+}
+
+/*var applyLanguage = function (lang) {  
 	alert('now language is: ' + lang);
 	var lang = document.getElementsByName('lang');
 		for (var i=0; i<lang.length; i++) {
 			if (lang[i].checked) {
-				localStorage.setItem(lang[i].value);
+				localStorage.setItem('myKey', lang[i].value);
 			}
 		}
-}
+}*/
 
 
 var getCurrentLanguage = function () {
 	var defaultLanguage = 'ua';
-	//localStorage.getItem();
-  
-  	return defaultLanguage;
+	
+	var langLocal = localStorage.getItem('myKey');
+	  	if(langLocal) {
+	  		return langLocal;
+	  	}
+	  	else {
+	  		return defaultLanguage;
+	  	}
 }
+
 
 var currentLang = getCurrentLanguage();
 var langEls = document.getElementsByClassName('lang-' + currentLang);
@@ -23,14 +37,8 @@ var langEls = document.getElementsByClassName('lang-' + currentLang);
 
 for (var i=0; i<langEls.length; i++) {
 	var langEl = langEls[i];
-	
-  // langEl.style.display = 'inline';
-  	langEl.classList.add('visible');
+	langEl.classList.add('visible');
 }
-
-
-
-
 
 // $<prefix> == DOMElement
 var $save = document.querySelector('#save');
