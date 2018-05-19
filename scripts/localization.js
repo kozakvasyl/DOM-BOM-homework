cdfe8c5adbe870494a18e890a78dde791ef16312
kozakvasyl <koretsua@gmail.com>
2018-05-19 03:32:08 +0300

    saved progres on task4

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,22 +1,12 @@
-var lang = document.getElementsByName('lang');
+/*var lang = document.getElementsByName('lang');
 		for (var i=0; i<lang.length; i++) {
 			lang[i].onchange = applyLanguage;
 		}
 
 function applyLanguage() {
+
 	localStorage.setItem('myKey', this.value);
 }
-
-/*var applyLanguage = function (lang) {  
-	alert('now language is: ' + lang);
-	var lang = document.getElementsByName('lang');
-		for (var i=0; i<lang.length; i++) {
-			if (lang[i].checked) {
-				localStorage.setItem('myKey', lang[i].value);
-			}
-		}
-}*/
-
 
 var getCurrentLanguage = function () {
 	var defaultLanguage = 'ua';
@@ -29,7 +19,6 @@ var getCurrentLanguage = function () {
 	  		return defaultLanguage;
 	  	}
 }
-
 
 var currentLang = getCurrentLanguage();
 var langEls = document.getElementsByClassName('lang-' + currentLang);
@@ -44,4 +33,40 @@ for (var i=0; i<langEls.length; i++) {
 var $save = document.querySelector('#save');
 $save.addEventListener('click', function(){
 	alert(1);
+});*/
+
+var lang = document.getElementsByName('lang');
+		for (var i=0; i<lang.length; i++) {
+			lang[i].onchange = applyLanguage;
+		}
+
+function applyLanguage(lang) {
+	/*var prevVisible = document.querySelectorAll('.visible');
+	prevVisible.classList.remove('visible');*/
+	var langEls = document.getElementsByClassName('lang-' + currentLang);
+	for (var i=0; i<langEls.length; i++) {
+		langEls[i].classList.add('visible');
+	}
+	console.log('in function: ', langEls);
+	localStorage.setItem('myKey', this.value);
+}
+
+var getCurrentLanguage = function () {
+	var defaultLanguage = 'ua';
+	
+	var langLocal = localStorage.getItem('myKey');
+	  	if(langLocal) {
+	  		return langLocal;
+	  	}
+	  	else {
+	  		return defaultLanguage;
+	  	}
+}
+
+var currentLang = getCurrentLanguage();
+var langEls = document.getElementsByClassName('lang-' + currentLang);
+console.log('first: ', langEls);
+var $save = document.querySelector('#save');
+$save.addEventListener('click', function(e){
+	localStorage.setItem('myKey', e.target.value);
 });
