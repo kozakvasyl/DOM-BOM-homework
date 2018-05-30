06e3b6746b4166706ddf6a10f0c950edb9b87919
kozakvasyl <koretsua@gmail.com>
2018-05-30 04:25:59 +0300

    i am working with localization.js file

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,17 +1,21 @@
-
-
+/*
 var lang = document.getElementsByName('lang');
 		for (var i=0; i<lang.length; i++) {
-			lang[i].onchange = applyLanguage;
+			lang[i].onclick = applyLanguage;
 		}
 
 function applyLanguage(lang) {
-	/*var prevVisible = document.querySelectorAll('.visible');
-	prevVisible.classList.remove('visible');*/
+	var prevVisible = document.querySelectorAll('.visible');
+	
+	for (var i =0; i<prevVisible.length; i++) {
+		prevVisible[i].classList.remove('visible');
+	}
+
 	var langEls= document.getElementsByClassName('lang-' + currentLang);
 	for (var i=0; i<langEls.length; i++) {
 		langEls[i].classList.add('visible');
-}
+	}
+
 	localStorage.setItem('myKey', this.value);
 }
 
@@ -32,5 +36,67 @@ var currentLang = getCurrentLanguage();
 var $save = document.querySelector('#save');
 $save.addEventListener('click', function(e){
 	localStorage.setItem('myKey', e.target.value);
-});
+});*/
 
+
+var lang = document.getElementsByName('lang');
+
+for (var i=0; i<lang.length; i++) {
+	lang[i].addEventListener('change', applyLanguage);
+};
+
+function applyLanguage() {
+	//delPrevLang();
+	localStorage.language = this.value;
+}
+
+var getCurrentLanguage = function () {
+	var defaultLanguage = 'ua';
+	
+	var langLocal = localStorage.language;
+	  	if(langLocal) {
+	  		return langLocal;
+	  	}
+	  	else {
+	  		return defaultLanguage;
+	  	}
+}
+var currentLang = getCurrentLanguage();
+
+function changeLanguage() {
+	var langEls= document.getElementsByClassName('lang-' + currentLang);
+	for (var i=0; i<langEls.length; i++) {
+		langEls[i].classList.add('visible');
+	}
+}
+changeLanguage();
+
+function delPrevLang() {
+	var prevLang = document.getElementsByClassName('lang');
+	for (var i = 0; i<prevLang.length; i++) {
+		prevLang[i].classList.remove('visible');
+	}
+}
+
+var $save = document.querySelector('#save');
+$save.addEventListener('click', function(e){
+	('language', e.target.value);
+	});
+
+
+/*var language='ua';
+var lang = document.getElementsByName('lang');
+for (var i=0; i<lang.length; i++) {
+	lang[i].addEventListener('change', function () {
+	language = this.value;
+	changeLanguage();
+	});
+}
+
+function changeLanguage() {
+	var langEls= document.getElementsByClassName('lang-' + language);
+	for (var i=0; i<langEls.length; i++) {
+		langEls[i].classList.add('visible');
+	}
+}
+*/
