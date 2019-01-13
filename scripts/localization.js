121e7660389bdada73cd4e2b99467ea3e841dc18
kozakvasyl <koretsua@gmail.com>
2019-01-13 23:05:27 +0200

    fixed default language in localization.js

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,44 +1,56 @@
 
 var lang = document.getElementsByName('lang');
 
-for (var i=0; i<lang.length; i++) {
-	lang[i].addEventListener('change', applyLanguage, changeLanguage);
+var langDefault = document.getElementsByClassName('lang-ua');
+
+for(var i =0; i<langDefault.length; i++) {
+	langDefault[i].classList.add('visible');
 };
 
+for (var i=0; i<lang.length; i++) {
+	lang[i].addEventListener('change', applyLanguage);
+};
+// main function_____________
 function applyLanguage() {
 	localStorage.language = this.value;
-}
 
-var getCurrentLanguage = function () {
-	var defaultLanguage = 'ua';
-	
-	var langLocal = localStorage.language;
-	  	if(langLocal) {
-	  		return langLocal;
-	  	}
-	  	else {
-	  		return defaultLanguage;
-	  	}
-}
-var currentLang = getCurrentLanguage();
-
-function changeLanguage(param) {
-	param();
-	var langEls= document.getElementsByClassName('lang-' + currentLang);
-	for (var i=0; i<langEls.length; i++) {
-		langEls[i].classList.add('visible');
+	// get/set value from localStorage_______________
+	var getCurrentLanguage = function () {
+		var defaultLanguage = 'ua';
+		
+		var langLocal = localStorage.language;
+			if(langLocal) {
+				return langLocal;
+			}
+			else {
+				return defaultLanguage;
+			}
 	}
-}
-changeLanguage(delPrevLang);
+	var currentLang = getCurrentLanguage();
 
-function delPrevLang() {
-	var prevLang = document.getElementsByClassName('lang');
-	for (var i = 0; i<prevLang.length; i++) {
-		prevLang[i].classList.remove('visible');
+	// change visibility on spans language___________
+	function changeVisibleLanguage(param) {
+		param();
+		var langEls= document.getElementsByClassName('lang-' + currentLang);
+		for (var i=0; i<langEls.length; i++) {
+			langEls[i].classList.add('visible');
+		}
 	}
-}
+	changeVisibleLanguage(delPrevLang);
 
-var $save = document.querySelector('#save');
-$save.addEventListener('click', function(e){
-	('language', e.target.value);
+	// remove visibility on spans language___________
+	function delPrevLang() {
+		var prevLang = document.getElementsByClassName('lang');
+		for (var i = 0; i<prevLang.length; i++) {
+			prevLang[i].classList.remove('visible');
+		}
+	};
+
+};
+
+// button event______________
+var save = document.querySelector('#save');
+save.addEventListener('click', function(e){
+
+	localStorage.getItem('language', e.target.value);
 });
